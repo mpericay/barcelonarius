9a6115db14e67f30d4f1822296d2be9ce7e63b98
Marti Pericay <marti.pericay@gmail.com>
2016-03-23 20:43:24 +0100

    create dummy slider

--- static/js/modules/ui.js
+++ static/js/modules/ui.js
@@ -5,20 +5,31 @@
 
 define(['map', 'bootstrap', 'slider'], function() {
 	
-	/*$("#slider").slider({
-		max: 20,
-		value: 12,
+	var isEven = function(n) {
+        n = Number(n);
+        return n === 0 || !!(n && !(n%2));
+      };
+    
+    var getTooltip = function(value) {
+        if(isEven(value)) return "Estiu 2014";
+        else return "Primavera 2014";
+    };
+    
+    var slider = $("#slider").slider({
+        formatter: function(value) {
+            return getTooltip(value);
+        },
+        tooltip: 'always',
+		max: 30,
+		value: 30,
 		step: 1,
-		ticks: [0, 4, 8, 12, 16, 20],
+		ticks: [0, 6, 12, 18, 24, 30],
 		ticks_labels: ['1979', '2002', '2005', '2008', '2011', '2014'],
 		ticks_snap_bounds: 0
 	});
-	
-	//won't work on mobile
-	$("#sliderContainer").on("mouseover", function() {
-		$(this).css('opacity', 1);
-	}).on("mouseout", function() {
-		$(this).css('opacity', 0.5);
-	});*/
+    
+    slider.change(function() {
+        //if (isEven(this.value)) $(".slider-handle").css("background-color", "#F00");
+    });
 	
 });
