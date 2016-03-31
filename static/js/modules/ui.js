083ed8a52946551e71ddaad775994400deed6689
Marti Pericay <marti.pericay@gmail.com>
2016-03-31 19:23:05 +0200

    slider with extra message support

--- static/js/modules/ui.js
+++ static/js/modules/ui.js
@@ -5,14 +5,145 @@
 
 define(['map', 'bootstrap', 'slider'], function() {
 	
-	var isEven = function(n) {
-        n = Number(n);
-        return n === 0 || !!(n && !(n%2));
-      };
+	var moments = [
+		{
+			year: 1979,
+			seasons: [0, 1], //default
+			dry: false
+		},
+		{
+			year: 1980,
+			dry: false
+		},
+		{
+			year: 1982,
+			dry: false
+		},
+		{
+			year: 1989,
+			dry: false
+		},
+		{
+			year: 1990,
+			dry: false
+		},
+		{
+			year: 1991,
+			dry: false
+		},
+		{
+			year: 1992,
+			dry: false
+		},
+		{
+			year: 1994,
+			dry: false
+		},
+		{
+			year: 1995,
+			dry: false
+		},
+		{
+			year: 1996,
+			dry: false
+		},
+		{
+			year: 1997,
+			dry: false
+		},
+		{
+			year: 1998,
+			dry: false
+		},
+		{
+			year: 1999,
+			dry: false
+		},
+		{
+			year: 2000,
+			dry: false
+		},
+		{
+			year: 2001,
+			dry: false
+		},
+		{
+			year: 2002,
+			dry: false
+		},
+		{
+			year: 2003,
+			dry: true
+		},
+		{
+			year: 2004,
+			dry: false
+		},
+		{
+			year: 2005,
+			dry: false
+		},
+		{
+			year: 2006,
+			dry: true
+		},
+		{
+			year: 2007,
+			dry: false
+		},
+		{
+			year: 2008,
+			dry: false
+		},
+		{
+			year: 2009,
+			dry: false
+		},
+		{
+			year: 2010,
+			dry: false
+		},
+		{
+			year: 2011,
+			dry: true
+		},
+		{
+			year: 2012,
+			dry: false
+		},
+		{
+			year: 2013,
+			dry: true
+		},
+		{
+			year: 2014,
+			dry: false
+		},
+		{
+			year: 2015,
+			dry: false
+		}
+	];
+	
+	var tickArray = [0, 5, 10, 15, 20, 28];
+	
+	var tickLabels = [];
+	tickArray.forEach(function(val) {
+		tickLabels.push(moments[val].year);	
+	});
+	
+	var isDry = function(val) {
+        return moments[val].dry;
+    };
     
     var getTooltip = function(value) {
-        if(isEven(value)) return "Estiu 2014";
-        else return "Primavera 2014";
+		var message = moments[value].year;
+		if(isDry(value)) {
+			$(".slider-handle").addClass("slider-handle-dry ");
+			message += " (sec)";
+		}
+		else $(".slider-handle").removeClass("slider-handle-dry ");			
+		return message;		
     };
     
     var slider = $("#slider").slider({
@@ -20,17 +151,17 @@ define(['map', 'bootstrap', 'slider'], function() {
             return getTooltip(value);
         },
         tooltip: 'always',
-		max: 30,
-		value: 30,
+		min: 0,
+		max: moments.length - 1,
+		value: moments.length - 1,
 		step: 1,
-		ticks: [0, 6, 12, 18, 24, 30],
-		ticks_labels: ['1979', '2002', '2005', '2008', '2011', '2014'],
+		ticks: tickArray,
+		ticks_labels: tickLabels,
 		ticks_snap_bounds: 0
 	});
     
     slider.change(function() {
-        if (isEven(this.value) && isEven(this.value/2)) $(".slider-handle").addClass("slider-handle-dry ");
-		else $(".slider-handle").removeClass("slider-handle-dry ");
+        
     });
 	
 });
